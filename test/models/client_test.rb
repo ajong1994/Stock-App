a95a24081ffa2908e8340cc6c9aea4d5859c9633
Alex Ong <aji@Alexs-MacBook-Pro.local>
2021-12-13 20:00:22 +0800

    Added Rspec to app

---
+++ test/models/client_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class ClientTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
+
+
+
+# 2. User
+# User Story #1: As a User, I want to create an account
+# User Story #2: As a User, I want to login my credentials so that I can access my account on the app
+# User Story #3: As a User, I want to receive an email after my registration to see if my registration is successful
+# User Story #4: As a User, I want to buy a stock from the stock market to add to my investment. Dropdown https://github.com/dblock/iex-ruby-client#get-symbols
+# User Story #5: As a User, I want to see my trading history
+# User Story #6: As a User, I want to receive an email to confirm my pending User Account signup. When admin approves my account
+# User Story #7: As a User, I want to sell the stocks I bought
+# User Story #8: As a User, I want to cash in (dummy
